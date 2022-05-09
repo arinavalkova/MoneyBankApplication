1311aeff523b0d200bb7ff8bb979fcb1e4f67081
Arina <sunny.qww@list.ru>
2022-05-09 15:24:56 +0700

    added folders infrastructure

---
+++ MoneyBankApplication/Views/Windows/AuthorizationWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace MoneyBankApplication.Views.Windows
+{
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_OnClick(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+    }
+}
---
+++ MoneyBankApplication/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace MoneyBankApplication.Views.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUp_OnClick(object sender, RoutedEventArgs e)
+        {
+            var authorizationWindow = new AuthorizationWindow();
+            authorizationWindow.Show();
+            Hide();
+        }
+    }
+}
